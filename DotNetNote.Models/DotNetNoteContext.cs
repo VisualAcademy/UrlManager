f728495f65ff79d5af43f3873edadc90321dab08
WebCampKR <webcamp@outlook.kr>
2019-04-21 08:21:19 +0900

    UrlManager 프로젝트 소스 처음 업로드

---
+++ DotNetNote.Models/DotNetNoteContext.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Configuration;
+
+namespace DotNetNote.Models
+{
+    public class DotNetNoteContext : DbContext
+    {
+        public DotNetNoteContext()
+        {
+            // Empty
+        }
+
+        public DotNetNoteContext(DbContextOptions<DotNetNoteContext> options)
+            : base(options)
+        {
+            // 공식과 같은 코드 
+        }
+
+        protected override void OnConfiguring(
+            DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                string connectionString = ConfigurationManager.ConnectionStrings[
+                    "ConnectionString"].ConnectionString;
+                optionsBuilder.UseSqlServer(connectionString);
+            }
+        }
+
+        /// <summary>
+        /// 도메인 관리자 테이블 참조
+        /// </summary>
+        public DbSet<Url> Urls { get; set; }
+    }
+}
